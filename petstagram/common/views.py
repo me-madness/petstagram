81d903614a47a5e6ae3445a4250337987c3d530e
me_madness <p_rangel@abv.bg>
2024-10-07 20:13:46 +0200

    "fix: all html files"

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
-
+from petstagram.photos.models import Photo
 # Create your views here.
 def home_page(request):
-    return render(request, 'common/home-page.html')
+
+    all_photos = Photo.objects.all()
+
+    context = {
+        'all_photos': all_photos
+    }
+
+    return render(request, 'common/home-page.html', context)
