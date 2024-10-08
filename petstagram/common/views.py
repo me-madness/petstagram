4c4b920ecb4f83dcf6e986b8db3a41e8ff190709
me_madness <p_rangel@abv.bg>
2024-10-08 18:13:45 +0200

    "feat: adding like func"

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from petstagram.photos.models import Photo
+from petstagram.common.models import Like
 # Create your views here.
 def home_page(request):
 
@@ -11,3 +12,16 @@ def home_page(request):
     }
 
     return render(request, 'common/home-page.html', context)
+def likes_functionality(request, photo_id: int):
+    liked_object = Like.objects.filter(
+        to_photo_id=photo_id
+    ).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo_id=photo_id)
+        like.save()
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
+
