01b18683b60db2229f2878d47b27fe01354d5573
me_madness <p_rangel@abv.bg>
2024-10-10 21:55:42 +0200

    "feat: details page"

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from petstagram.pets.models import Pet
+
 
 # Create your views here.
 def pet_add_page(request):
@@ -15,4 +17,11 @@ def pet_delete_page(request, username: str, pet_slug: str):
 
 
 def pet_details_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-details-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+
+    context = {
+        'pet': pet,
+        'all_photos': all_photos,
+    }
+    return render(request, 'pets/pet-details-page.html', context)
